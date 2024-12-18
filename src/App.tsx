c63c840ec6459493dfc9e87ef629d2835bb391a9
Leonardo <leonardo.palvarenga@gmail.com>
2024-12-18 11:12:53 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen relative">
-      <h1 className="text-[200px] leading-[100px] uppercase font-extrabold">
+    <div className="flex justify-center items-center w-screen min-h-screen relative">
+      <div className="text-[220px] leading-[100px] uppercase font-black max-md:text-6xl max-md:leading-10 select-none">
         <p className="text-zinc-950">lorem</p>
-        <p className="translate-x-20 text-zinc-800">ipsum</p>
+        <p className="translate-x-20 text-zinc-800 max-md:translate-x-10">ipsum</p>
         <p className="text-zinc-500">dolor</p>
-      </h1>
+      </div>
       <div className="absolute right-2 bottom-2 flex gap-3">
         <a href="https://www.linkedin.com/in/leonardo-prado-846496298/">
           <FaLinkedin size={32}/>
