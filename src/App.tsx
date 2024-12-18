ae73f0fb3943683285242c512444c03f7a1a800d
Leonardo <leonardo.palvarenga@gmail.com>
2024-12-18 11:05:42 -0300

    add Tailwind CSS setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import shrek from './assets/shrek.jpg'
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-    <img src={shrek} alt="" />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="flex justify-center items-center h-screen relative">
+      <h1 className="text-[200px] leading-[100px] uppercase font-extrabold">
+        <p className="text-zinc-950">lorem</p>
+        <p className="translate-x-20 text-zinc-800">ipsum</p>
+        <p className="text-zinc-500">dolor</p>
+      </h1>
+      <div className="absolute right-2 bottom-2 flex gap-3">
+        <a href="https://www.linkedin.com/in/leonardo-prado-846496298/">
+          <FaLinkedin size={32}/>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://github.com/LeoAlvarenga404">
+          <FaGithub size={32}/>
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
